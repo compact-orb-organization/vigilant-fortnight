44c93f5a8c4ad5000b679df429612044abfcc2f9
compact-orb <compact-orb@outlook.com>
2025-04-18 03:17:31 +0000

    Initial commit

---
+++ emerge.sh
@@ -0,0 +1,33 @@
+# Copy custom locale definitions into /etc and rebuild the locale database
+cp /root/workspace/locale.gen /etc/
+locale-gen
+# Select the desired locale entry
+eselect locale set 6
+
+# Deploy all Portage configuration files from the workspace
+cp --recursive /root/workspace/portage/* /etc/portage/
+
+# Persist S3 credentials into a script for later use by rclone
+echo s3_access_key_id=$S3_ACCESS_KEY_ID >> /etc/portage/s3.sh
+echo s3_endpoint=$S3_ENDPOINT >> /etc/portage/s3.sh
+echo s3_secret_access_key=$S3_SECRET_ACCESS_KEY >> /etc/portage/s3.sh
+echo s3_bucket=$S3_BUCKET >> /etc/portage/s3.sh
+
+# Download and install rclone for interacting with S3 storage
+wget --directory-prefix=/tmp https://downloads.rclone.org/v1.69.1/rclone-v1.69.1-linux-amd64.zip
+unzip /tmp/rclone-v1.69.1-linux-amd64.zip rclone-v1.69.1-linux-amd64/rclone -d /tmp
+mv /tmp/rclone-v1.69.1-linux-amd64/rclone /usr/local/bin
+rm --recursive /tmp/rclone-v1.69.1-linux-amd64*
+
+# Refresh the Portage tree from Gentoo mirrors
+emerge-webrsync
+
+# Select the desired profile
+eselect profile set 26
+
+# Load S3 credentials and download Packages to binpkgs
+source /etc/portage/s3.sh
+rclone --config /etc/portage/rclone.conf --s3-access-key-id $s3_access_key_id --s3-endpoint $s3_endpoint --s3-secret-access-key $s3_secret_access_key copy 1:$s3_bucket/Packages /var/cache/binpkgs/
+
+# Re-emerge all previously installed packages
+emerge @installed
