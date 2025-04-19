c0b517847c92d8db811f224988c82e63219f3db8
compact-orb <compact-orb@outlook.com>
2025-04-19 00:25:50 +0000

    Rewrite

--- emerge.sh
+++ emerge.sh
@@ -14,11 +14,14 @@ eselect profile set 26
 # Deploy all Portage configuration files from the workspace
 cp --recursive /root/workspace/portage/* /etc/portage/
 
-# Create the binrepos.conf file for Portage
+# Configure binary package handling
 if [[ ! " $* " =~ " --no-binpkg " ]]; then
+    # Remove default Gentoo binhost to use custom S3 bucket
     rm /etc/portage/binrepos.conf/gentoobinhost.conf
+    # Substitute actual bucket name into custom binrepos config
     sed --in-place "s|\$S3_BUCKET|${S3_BUCKET}|g" /etc/portage/binrepos.conf/vigilant-fortnight.conf
 else
+    # Disable binary package builds by removing getbinpkg from make.conf
     sed --in-place "s/ getbinpkg//g" /etc/portage/make.conf
 fi
 
@@ -28,7 +31,9 @@ unzip /tmp/rclone-v1.69.1-linux-amd64.zip rclone-v1.69.1-linux-amd64/rclone -d /
 mv /tmp/rclone-v1.69.1-linux-amd64/rclone /usr/local/bin
 rm --recursive /tmp/rclone-v1.69.1-linux-amd64*
 
+# Install fuse (needed for rclone mount) and then mount the remote binpkg cache
 FEATURES="-buildpkg -getbinpkg" emerge sys-fs/fuse
+# Mount S3 bucket to /var/cache/binpkgs using rclone
 rclone --config /etc/portage/rclone.conf --s3-access-key-id $S3_ACCESS_KEY_ID --s3-endpoint $S3_ENDPOINT --s3-secret-access-key $S3_SECRET_ACCESS_KEY mount 1:$S3_BUCKET /var/cache/binpkgs --allow-other --daemon
 
 # Re-emerge all previously installed packages
