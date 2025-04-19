0d4871a580cd2b521adb83a0910b442b00332001
compact-orb <compact-orb@outlook.com>
2025-04-19 00:22:07 +0000

    Rewrite

--- emerge.sh
+++ emerge.sh
@@ -1,24 +1,25 @@
 # Copy custom locale definitions into /etc and rebuild the locale database
 cp /root/workspace/locale.gen /etc/
 locale-gen
+
 # Select the desired locale entry
 eselect locale set 6
+
+# Refresh the Portage tree from Gentoo mirrors
+emerge-webrsync
+
+# Select the desired profile
+eselect profile set 26
 
 # Deploy all Portage configuration files from the workspace
 cp --recursive /root/workspace/portage/* /etc/portage/
 
-# Persist S3 credentials into a script for later use by rclone
-echo s3_access_key_id=$S3_ACCESS_KEY_ID >> /etc/portage/s3.sh
-echo s3_endpoint=$S3_ENDPOINT >> /etc/portage/s3.sh
-echo s3_secret_access_key=$S3_SECRET_ACCESS_KEY >> /etc/portage/s3.sh
-echo s3_bucket=$S3_BUCKET >> /etc/portage/s3.sh
-
 # Create the binrepos.conf file for Portage
 if [[ ! " $* " =~ " --no-binpkg " ]]; then
     rm /etc/portage/binrepos.conf/gentoobinhost.conf
-    sed -i "s|\$S3_BUCKET|${S3_BUCKET}|g" /etc/portage/binrepos.conf/vigilant-fortnight.conf
+    sed --in-place "s|\$S3_BUCKET|${S3_BUCKET}|g" /etc/portage/binrepos.conf/vigilant-fortnight.conf
 else
-    sed --in-place 's/ getbinpkg//g' /etc/portage/make.conf
+    sed --in-place "s/ getbinpkg//g" /etc/portage/make.conf
 fi
 
 # Download and install rclone for interacting with S3 storage
@@ -27,14 +28,8 @@ unzip /tmp/rclone-v1.69.1-linux-amd64.zip rclone-v1.69.1-linux-amd64/rclone -d /
 mv /tmp/rclone-v1.69.1-linux-amd64/rclone /usr/local/bin
 rm --recursive /tmp/rclone-v1.69.1-linux-amd64*
 
-# Refresh the Portage tree from Gentoo mirrors
-emerge-webrsync
-
-# Select the desired profile
-eselect profile set 26
-
-# Load S3 credentials and download Packages to binpkgs
-
+FEATURES="-buildpkg -getbinpkg" emerge sys-fs/fuse
+rclone --config /etc/portage/rclone.conf --s3-access-key-id $S3_ACCESS_KEY_ID --s3-endpoint $S3_ENDPOINT --s3-secret-access-key $S3_SECRET_ACCESS_KEY mount 1:$S3_BUCKET /var/cache/binpkgs --allow-other --daemon
 
 # Re-emerge all previously installed packages
 emerge @installed
