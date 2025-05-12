dcac735f1f2d39226b82be7379d96a091dea1c31
compact-orb <compact-orb@outlook.com>
2025-05-12 22:54:29 +0800

    Update

--- 1/emerge.sh
+++ 1/emerge.sh
@@ -5,6 +5,9 @@ set -eo pipefail
 # Set the date for the Portage snapshot.
 portage_snapshot_date="20250504"
 
+# Sync the main Gentoo ebuild repository using emerge-webrsync.
+emerge-webrsync --revert=$portage_snapshot_date --quiet
+
 if [ "$2" = first ]; then
     # Generate the specified locales.
     locale-gen --quiet
@@ -15,9 +18,6 @@ if [ "$2" = first ]; then
     # Set the Gentoo profile
     eselect --brief profile set 26
 fi
-
-# Sync the main Gentoo ebuild repository using emerge-webrsync.
-emerge-webrsync --revert=$portage_snapshot_date --quiet
 
 # Emerge the packages passed as the first argument ($1) to the script, with a timeout.
 timeout 18000 emerge $1
