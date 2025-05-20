9f3fcb8b2192ae26a7ec54adcf8d534f5da6705c
compact-orb <compact-orb@outlook.com>
2025-05-20 21:15:17 +0800

    Update

--- 3/emerge.sh
+++ 3/emerge.sh
@@ -20,7 +20,7 @@ if [ "$2" = first ]; then
 fi
 
 # Emerge the packages passed as the first argument ($1) to the script, with a timeout.
-timeout 18900 emerge $1
+timeout 19800 emerge $1
 
 # Remove orphaned dependencies.
 emerge --depclean
